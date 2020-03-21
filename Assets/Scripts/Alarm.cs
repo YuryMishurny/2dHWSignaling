227eae72ee6962df6d078303d0ff97d2c20ea0dd
yurabgas2k2 <60823265+yurabgas2k2@users.noreply.github.com>
2020-03-21 20:40:34 +0300

    Commit

---
+++ Assets/Scripts/Alarm.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Alarm : MonoBehaviour
+{
+    [SerializeField] private SpriteRenderer _renderer;
+    [SerializeField] private Color _color;
+    [SerializeField] private Color _targetColor;
+    [SerializeField] private AudioSource _audio;
+
+    private void Update()
+    {
+        ChangeVolume();
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.TryGetComponent<MoveThief>(out MoveThief moveThief))
+        {
+            _renderer.color = _targetColor;
+            _audio.Play();           
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        _renderer.color = _color;
+        _audio.Stop();
+    }
+    private void ChangeVolume()
+    {
+        _audio.volume = Mathf.Sin(Time.time * 3);
+    }
+}
---
+++ Assets/Scripts/AudioVolumeChanger.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioVolumeChanger : MonoBehaviour
+{
+    [SerializeField] private AudioSource _audio;
+
+    void Update()
+    {
+        ChangeVolume();
+    }
+
+    private void ChangeVolume()
+    {
+        _audio.volume = Mathf.Sin(Time.time*3);
+    }
+}
